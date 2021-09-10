842828ca545d30b81c6a1dcdb1c0a51ed800cd72
Steven Simms <simmsstev@gmail.com>
2021-09-09 19:04:55 -0700

    Created Comment Model
    Created Comment Model and assigned Navigation properties

---
+++ Models/Comment.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SteveSimmsCodesBlog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+
+        public string ModeratorId { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public DateTime? Moderated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Moderated Comment")]
+        public string ModeratedBody { get; set; } // When the moderator changes the user comments 
+
+        //Navigation properties levrages the foreign key to get the entire record 
+        public virtual Post Post { get; set; } // hold the entire record of the AuthorId string 
+
+
+        public virtual IdentityUser Author { get; set; } // Represents the default id of the user in our system tied to our foreign key Author 
+
+        public virtual IdentityUser Moderator { get; set; } // purpose of this is to go from the moderator id string to the (IdentityUser)moderator record 
+    }
+}
