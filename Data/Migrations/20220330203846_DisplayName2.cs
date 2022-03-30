052ebf81e672b9041210314c55cf4b872541dfb1
SteveSimms <simmsstev@gmail.com>
2022-03-30 13:50:24 -0700

    Added Programatic migrations

---
+++ Data/Migrations/20220330203846_DisplayName2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SteveSimmsCodesBlog.Data.Migrations
+{
+    public partial class DisplayName2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
