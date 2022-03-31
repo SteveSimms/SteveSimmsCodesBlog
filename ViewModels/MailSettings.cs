14045bd94ecb1128830fb4cad7489065bbf51eef
SteveSimms <simmsstev@gmail.com>
2022-03-31 06:24:18 -0700

    added view models

---
+++ ViewModels/MailSettings.cs
@@ -0,0 +1,12 @@
+namespace SteveSimmsCodesBlog;
+
+public class MailSettings
+{
+    //So we can Configure and use and smtp server
+    //from google for example
+    public string Mail { get; set; }
+    public string DisplayName { get; set; }
+    public string Password { get; set; }
+    public string Host { get; set; }
+    public string  Port  { get; set; }
+}
